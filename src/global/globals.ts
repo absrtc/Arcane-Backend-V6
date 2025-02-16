00b7389d292e5d201cf49e370c7283ce94187d5e
absrtc <pixelstrap9@gmail.com>
2025-02-16 12:16:49 -0700

    Delete src directory

--- src/global/globals.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const globals = {
-    BackendName: process.env.NAME || "Arcane",
-    ConnectionUri: process.env.DATABASE || "mongodb://localhost:27017/Arcane",
-    PORT: process.env.PORT || 3551
-};
